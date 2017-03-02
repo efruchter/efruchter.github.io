bfe53eebc1ae655f48f6233141b79761ff36bd53
efruchter <fruchter.eric@gmail.com>
2017-03-01 23:01:28 -0800

    test

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   var navs = '<center>\
   <h1>Eric Fruchter</h1>\
-  <canvas class="birdcanvas" width="500" height="100"></canvas>\
+  <img class="kazoo" img src="images/468480.jpg" alt="kawanabe kyosai">\
   <ul class="horizontal">\
   <li class="titles" id="aboutTitle"><a href="index.html"><h3>About</h3></a></li>\
   <li class="titles" id="professionalTitle"><a href="professional.html"><h3>Professional</h3></a></li>\
@@ -12,17 +12,4 @@ $(document).ready(function(){
   </center>';
 
   $('#navbar').html(navs);
-
-  $("canvas.birdcanvas").click(function(){
-    $(this).fadeTo('slow', 0, function() {
-      drawBirds(this, true, "#3B3131");
-      $(this).fadeTo('slow', 1);
-    });
-  });
-
-  $("canvas").trigger('click');
-
-  $(".titles").fadeTo('slow', .6);
-  $("div.content").hide();
-  $("div.content").fadeTo('slow', 1);
 });
